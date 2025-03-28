8879b7dbc39e38b32e85a728888ad85e45f6a4b6
Gerard Arnaiz Sanchez <astor98gas@proton.me>
2025-03-28 13:44:42 +0100

    first commit

---
+++ BackEnd/src/test/java/com/arsansys/RemaPartners/RemaPartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsansys.RemaPartners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemaPartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
